a9693d6c44b9bbda9e39fa5fb855ff99a46cb3cb
Haoran Sun <254664427@qq.com>
2019-02-24 17:32:25 +1100

    Divid some dataset
    Just divided some dataset

--- Decision_tree.py
+++ Decision_tree.py
@@ -1,5 +1,14 @@
-# regression decision tree
+# XG_boosting
+import  numpy as np
+import pandas as pd
+import xgboost as xgb
+
+# dtrain = xgb.DMatrix('train.csv?format=csv&label_column=0')
+# dtest = xgb.DMatrix('test.csv?format=csv&label_column=0')
+data = pd.read_csv('heart.csv')
+train = data[250:]
+test = data[:55]
+train.to_csv("train.csv")
+test.to_csv("test.csv")
 
 
-
-print('109')
